dc9b621e4ac01ef6d2c214ce2c78f5fed5192436
JavelFreitas <javelqueiroz@outlook.com>
2019-11-10 19:28:05 -0300

    O problema era a segunda requisição em busca do favicon.ico, tratando esse "erro" o código funcionou

--- Python/Socket/Códigos/HTTP Multithreading/TCP_HTTP_Socket.py
+++ Python/Socket/Códigos/HTTP Multithreading/TCP_HTTP_Socket.py
@@ -11,35 +11,41 @@ serverSocket = socket(AF_INET, SOCK_STREAM)
 
 #codigo_inicio
 HOST = ''
-PORT = 8088
+PORT = 8086
 serverSocket.bind((HOST, PORT))
 serverSocket.listen(10)
 #codigo_fim
 
 
-def responderCliente(message):
+def responderCliente(connectionSocket):
     try:
-        filename = message.decode().split()[1]
-        f = open(filename[1:])
-        outputdata = f.readlines()
-        #codigo_inicio
-        #codigo_fim
+        message = connectionSocket.recv(1024)
+        # print("=====================")
+        # print(message)
+        # print("=====================")
+        message = message.decode()
+        filename = message.split()[1]
         
-        #Envia um linha de cabeçalho HTTP para o socket
-        #codigo_inicio
-        connectionSocket.send('HTTP/1.0 200 OK \n'.encode())
-        connectionSocket.send('Content-type:text/html \n\n'.encode())
-        #codigo_fim
-        #Envia o conteúdo do arquivo solicitado ao cliente
-        for i in range(0, len(outputdata)):
-            connectionSocket.send(outputdata[i].encode())
-        connectionSocket.send("\r\n".encode())
-        connectionSocket.close()    
+        if filename[1:] != "favicon.ico":
+            f = open(filename[1:])
+            outputdata = f.readlines()
+            #codigo_inicio
+            #codigo_fim
+            
+            #Envia um linha de cabeçalho HTTP para o socket
+            #codigo_inicio
+            connectionSocket.send('HTTP/1.0 200 OK \n'.encode())
+            connectionSocket.send('Content-type:text/html \n\n'.encode())
+            #codigo_fim
+            #Envia o conteúdo do arquivo solicitado ao cliente
+            for i in range(0, len(outputdata)):
+                connectionSocket.send(outputdata[i].encode())
+            connectionSocket.send("\r\n".encode())
+            connectionSocket.close()    
         pass
     except IOError:
         #Envia uma mensagem de resposta “File not Found”
 
-        #codigo_inicio
         connectionSocket.send('HTTP/1.0 404\n'.encode())
         connectionSocket.send('Content-type: text/html\n\n'.encode())
         connectionSocket.send('<html lang="en">'.encode())
@@ -47,25 +53,29 @@ def responderCliente(message):
         connectionSocket.send('404 - File Not Found'.encode())
         connectionSocket.send('</body>'.encode())
         connectionSocket.send('</html>\r\n'.encode())
-        #codigo_fim
 
-        #Fecha o socket cliente
-        #codigo_inicio
         connectionSocket.close()
-        #codigo_fim
-        serverSocket.close()
-        sys.exit()
-    except OSError:
+
+    except FileNotFoundError:
+        print("File not Found")
+        connectionSocket.send('HTTP/1.0 404\n'.encode())
+        connectionSocket.send('Content-type: text/html\n\n'.encode())
+        connectionSocket.send('<html lang="en">'.encode())
+        connectionSocket.send('<body>'.encode())
+        connectionSocket.send('404 - File Not Found'.encode())
+        connectionSocket.send('</body>'.encode())
+        connectionSocket.send('</html>\r\n'.encode())
+
         connectionSocket.close()
-        serverSocket.close()
-        sys.exit()
 
 while True:
     #Estabelece a conexão
     print('Ready to serve...')
-
-    connectionSocket, addr = serverSocket.accept()
-    print("Aceita a coneccao e comeca uma nova thread")
-    start_new_thread(responderCliente, (connectionSocket.recv(1024), ))
+    try:
+        connectionSocket, addr = serverSocket.accept()
+        print("@   Comecando uma thread")
+        start_new_thread(responderCliente, (connectionSocket, ))
+    except OSError:
+        serverSocket.close()
 serverSocket.close()
 sys.exit() #Termina o programa depois de enviar os dados
