147efba4f53ab9061ac25e13ed11bddb6f78ce3c
Javel Queiroz <javelqueiroz@outlook.com.br>
2019-12-07 16:04:59 -0300

    Menu is working

---
+++ Python/Challanges/Challenge 1/basic_calculator.py
@@ -0,0 +1,27 @@
+def sum(a, b):
+    return a+b
+    pass
+
+def decrease(c, d):
+    return c-d
+    pass
+
+def divide(e, f):
+    return e/f
+    pass
+
+def multiply(g, h):
+    return g*h
+    pass
+
+def menu():
+    print("Welcome to a simple calculator!")
+    print("We have the following options available:")
+    print("0 - Quit calculator")
+    pass
+
+option = 1
+while option != 0:
+    menu()
+    option = input("")
+    
