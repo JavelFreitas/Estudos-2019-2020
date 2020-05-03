df58be9444ecdf20ac573ebc186ad9bde212e040
Javel Queiroz <javelqueiroz@outlook.com.br>
2020-05-02 21:53:13 -0300

    Seguindo as aulas da Rocketseat mas mudando um pouco. final da aula 2.

---
+++ Javascript/App de Todos/functions.js
@@ -0,0 +1,23 @@
+var listElement = document.querySelector("#app ul");
+var inputElement = document.querySelector("#app input#Input");
+var buttonElement = document.querySelector("#app button#form1Button");
+
+var todos = [
+    'Fazer café',
+    'Estudar Javascript',
+    'Acessar comunidade da Rocketseat'
+];
+
+function renderTodos() {
+
+    for (todo of todos) {
+        console.log(todo);
+        
+        var todoElement = document.createElement('li');
+        var todoText = document.createTextNode(todo);
+        
+        todoElement.appendChild(todoText);
+        listElement.appendChild(todoElement);
+    }
+}
+renderTodos();
