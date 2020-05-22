452102308b4c673e080e06c5618d4ca919ffe9c9
Javel Queiroz <javelqueiroz@outlook.com.br>
2020-05-22 11:21:37 -0300

    Understanding filter function

--- Javascript/ES6/Array Manipulation/arrayExamples.js
+++ Javascript/ES6/Array Manipulation/arrayExamples.js
@@ -1,7 +1,7 @@
 window.addEventListener('load', () => {
     let vector = [1, 2, 3, 4, 5, 6, 7];
     mapExample(vector);
-
+    filterExample(vector);
 
 });
 
@@ -10,4 +10,9 @@ window.addEventListener('load', () => {
 function mapExample(vector){
     console.log(vector.map(number => {return number + 1;}));
     // for each number inside of the vector the function on the right side is being used
+}
+
+function filterExample(vector){
+    console.log(vector.filter(num => {return num > 3;}))
+    // returns a new array with the elements that are suitable to the conditions
 }
