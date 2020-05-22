49c0cb119880e63d888257e3a5fb7ee364024ee9
Javel Queiroz <javelqueiroz@outlook.com.br>
2020-05-22 10:16:07 -0300

    Final improvements

--- Javascript/JS assíncrono/index.js
+++ Javascript/JS assíncrono/index.js
@@ -4,22 +4,26 @@ var input1 = document.querySelector("input#input1") || [];
 function checaIdade(idade) {
     return new Promise(function (resolve, reject) {
         if (idade > 18) {
-            
-            setTimeout(resolve, 5000);
+            setTimeout(resolve, 1000);
         } else {
-            setTimeout(reject, 2000);
+            setTimeout(reject, 500);
         }
-    })
+    });
 }
 
-button1.onclick = checaIdade(input1.value)
-.then(function () {
-    alert("resolve");
-    document.querySelector('h1').innerHTML = "Tudo certo";
-    console.log("Maior que 18");
-})
-.catch(function () {
-    document.querySelector('h1').innerHTML = "Algo errado";
-    console.log("Menor que 18");
-});;
-    
+
+button1.onclick = function () {
+    var idade = input1.value;
+    input1.value = '';
+    checaIdade(idade)
+        .then(function () {
+            document.querySelector('h1').innerHTML = "Tudo certo(Maior que 18)";
+            console.log("Maior que 18");
+        })
+        .catch(function () {
+            document.querySelector('h1').innerHTML = "Algo errado(Menor que 18)";
+            console.log("Menor que 18");
+        });
+
+}
+
