217a657831cbfadbfdc7f392e75f8811b4cf6507
Javel Queiroz <javelqueiroz@outlook.com.br>
2020-05-22 23:54:40 -0300

    Learning how to search for a name (Still have to improve it) Covered 4 cases - Without text - The input - First Letter Upper - All text Upper - All text Lower

---
+++ Javascript/ES6/Array Manipulation/temp.js
@@ -0,0 +1,21 @@
+window.addEventListener('load', () => {
+    const arr = ['Ana', 'Julia', 'Matheus', 'Javel', 'Julio', 'Livia', 'Lyse', 'Viviane'];
+    let inputArray = "";
+    if (!inputArray){
+        console.log("Empty input");
+        return;
+    }
+    let inputFirstUpper = inputArray;
+    inputFirstUpper = inputFirstUpper[0].toUpperCase() + inputFirstUpper.slice(1);
+    let inputUpper = inputArray.toUpperCase();
+    let inputLower = inputArray.toLowerCase();
+    
+    // console.log(inputUpper);
+    // console.log(inputLower);
+    // console.log(inputFirstUpper[0]);
+
+    console.log(arr.filter((element) => { return (element.indexOf(inputArray) !== -1 
+        || element.indexOf(inputUpper) !== -1
+        || element.indexOf(inputLower) !== -1
+        || element.indexOf(inputFirstUpper) !== -1) }));
+});
