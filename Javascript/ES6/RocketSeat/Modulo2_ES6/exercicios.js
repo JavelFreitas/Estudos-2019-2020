4dcd65e49d3fb448b982e7e5ffccddaf38fef3a1
Javel Queiroz <javelqueiroz@outlook.com.br>
2020-06-26 17:22:16 -0300

    Finished exercises

---
+++ Javascript/ES6/RocketSeat/Modulo2_ES6/exercicios.js
@@ -0,0 +1,14 @@
+export const idade = 23;
+
+export default class Usuario {
+ static info() {
+ console.log('Apenas teste');
+ }
+}
+
+/* 
+import ClasseUsuario, {idade as IdadeUsuario} from '../Modulo2_ES6/exercicios'; 
+
+ClasseUsuario.info();
+console.log(IdadeUsuario);
+*/
--- Javascript/ES6/RocketSeat/src/main.js
+++ Javascript/ES6/RocketSeat/src/main.js
@@ -1,1 +1,5 @@
-alert('opa');
+import ClasseUsuario, {idade as IdadeUsuario} from '../Modulo2_ES6/exercicios'; 
+
+
+ClasseUsuario.info();
+console.log(IdadeUsuario);
