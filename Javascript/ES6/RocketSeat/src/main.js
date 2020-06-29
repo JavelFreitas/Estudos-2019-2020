12203f8ac902e908695a318fc97b335e769dba83
Javel Queiroz <javelqueiroz@outlook.com.br>
2020-06-29 18:27:35 -0300

    Added render

--- Javascript/ES6/RocketSeat/src/main.js
+++ Javascript/ES6/RocketSeat/src/main.js
@@ -3,6 +3,7 @@ class App {
     constructor(){
         this.repositories = [];
         this.formEl = document.getElementById('repo-form');
+        this.listEl = document.getElementById('repo-list');
         this.registerHandlers();
     }
 
@@ -18,8 +19,30 @@ class App {
             html_url: 'http://github.com/rocketseat'
         })
 
-        console.log(this.repositories);
+        this.render();
+    }
+
+    render(){
+        this.listEl.innerHTML = '';
         
+        this.repositories.forEach(repo => {
+            let imgEl = document.createElement('img');
+            imgEl.setAttribute('src', repo.avatar_url);
+
+            let titleEl = document.createElement('strong');
+            titleEl.appendChild(document.createTextNode(repo.name));
+
+            let linkEl = document.createElement('a');
+            linkEl.setAttribute('target', '_blank');
+            linkEl.appendChild(document.createTextNode('Acessar'));
+
+            let listItemEl = document.createElement('li');
+            listItemEl.appendChild(imgEl);
+            listItemEl.appendChild(titleEl);
+            listItemEl.appendChild(linkEl);
+
+            this.listEl.appendChild(listItemEl);
+        });
     }
 }
 
