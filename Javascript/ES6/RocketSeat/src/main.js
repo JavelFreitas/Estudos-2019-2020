9b15e11295659c573eece2c04621b0cb8de808a2
Javel Queiroz <javelqueiroz@outlook.com.br>
2020-07-13 21:07:09 -0300

    Finishing ES6 classes

--- Javascript/ES6/RocketSeat/src/main.js
+++ Javascript/ES6/RocketSeat/src/main.js
@@ -12,6 +12,18 @@ class App {
         this.formEl.onsubmit = event => this.addRepository(event);   
     }
 
+    setLoading(loading = true){
+        if (loading === true){
+            let loadingEl = document.createElement('span');
+            loadingEl.appendChild(document.createTextNode('Carregando...'));
+            loadingEl.setAttribute('id', 'loading');
+
+            this.formEl.appendChild(loadingEl);
+        }else{
+            document.getElementById('loading').remove();
+        }
+    }
+
     async addRepository(event){
         event.preventDefault();
 
@@ -19,19 +31,25 @@ class App {
 
         if (repoInput.length === 0)
             return;
-        
-        const response = await api.get(`/repos/${repoInput}`);
-        
-        const {name, html_url, owner:{ avatar_url } } =response.data;
-        this.repositories.push({
-            name,
-            avatar_url,
-            html_url
-        })
+        this.setLoading();
+        try{
 
-        this.inputEl.value = '';
-
-        this.render();
+            const response = await api.get(`/repos/${repoInput}`);
+            
+            const {name, html_url, owner:{ avatar_url } } =response.data;
+            this.repositories.push({
+                name,
+                avatar_url,
+                html_url
+            })
+            
+            this.inputEl.value = '';
+            
+            this.render();
+        }catch(error){
+            alert('O repositorio nao existe');
+        }
+        this.setLoading(false);
     }
 
     render(){
